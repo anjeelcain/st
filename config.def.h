8c3e0f266ce97039aa0cc3d3944bb52ca5fa2265
Anjeel <anjeelzero@tuta.io>
2023-07-09 04:46:53 -0300

    style: pnevma colorscheme

--- config.def.h
+++ config.def.h
@@ -107,45 +107,31 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  [0] = "#2f2e2d", /* black   */
+  [1] = "#a36666", /* red     */
+  [2] = "#90a57d", /* green   */
+  [3] = "#d7af87", /* yellow  */
+  [4] = "#7fa5bd", /* blue    */
+  [5] = "#c79ec4", /* magenta */
+  [6] = "#8adbb4", /* cyan    */
+  [7] = "#d0d0d0", /* white   */
+  [8]  = "#4a4845", /* black   */
+  [9]  = "#d78787", /* red     */
+  [10] = "#afbea2", /* green   */
+  [11] = "#e4c9af", /* yellow  */
+  [12] = "#a1bdce", /* blue    */
+  [13] = "#d7beda", /* magenta */
+  [14] = "#b1e7dd", /* cyan    */
+  [15] = "#efefef", /* white   */
+  [255] = 0,
+  [256] = "#1c1c1c", /* background */
+  [257] = "#dddddd", /* foreground */
 };
 
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 /*
  * Default shape of cursor
  * 2: Block ("█")
